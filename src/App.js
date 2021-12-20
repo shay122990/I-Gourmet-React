894723c356e2d22cd8d6970b6c6267b5a5ae4dc8
Shay.Asanova <shay.asanova@gmail.com>
2021-12-21 02:03:55 +0400

    app setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css"
+import Navbar from "./components/Navbar"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
+function App() {
+  return (
+    <div className='App'>
+      <Router>
+        <Navbar />
+      </Router>
+    </div>
+  )
+}
+
+export default App
