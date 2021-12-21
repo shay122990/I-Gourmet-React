b98a12a0270d856e32ea8e4f16d248410686755f
Shay.Asanova <shay.asanova@gmail.com>
2021-12-22 03:09:36 +0400

    About, Contact, style

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
 import Footer from "./components/Footer"
 import Menu from "./pages/Menu"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Routes>
           <Route path='/' exact element={<Home />} />
           <Route path='/menu' exact element={<Menu />} />
+          <Route path='/about' exact element={<About />} />
+          <Route path='/contact' exact element={<Contact />} />
         </Routes>
         <Footer />
       </Router>
