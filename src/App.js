58ef52d468eb6ba7702666d2db061e3b96b8be95
Shay.Asanova <shay.asanova@gmail.com>
2023-02-20 19:36:06 +0400

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import "./App.css"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Navbar from "./components/Navbar"
-import Home from "./pages/Home"
-import Footer from "./components/Footer"
-import Menu from "./pages/Menu"
-import About from "./pages/About"
-import Contact from "./pages/Contact"
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import Menu from "./pages/Menu";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' exact element={<Home />} />
+          <Route path='/*' exact element={<Home />} />
           <Route path='/menu' exact element={<Menu />} />
           <Route path='/about' exact element={<About />} />
           <Route path='/contact' exact element={<Contact />} />
@@ -21,7 +21,7 @@ function App() {
         <Footer />
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
